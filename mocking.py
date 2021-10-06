c41f08c181137fea882dc06383ae7c909a0b1a21
Nicholas Mirarchi <nmirarchi12@gmail.com>
2021-10-06 16:52:23 -0400

    updating mocking script with additional test

--- mocking.py
+++ mocking.py
@@ -21,10 +21,28 @@ class TestHw4(unittest.TestCase):
         response = repository_info(Github)
         #assert_list_equal(response.json(),myJson)
         assert_is_not_none(response)
+        
+    @patch('gitRequest.repository_info')
+    def test_repositorys(self, mock_repository_info):
+        mock_repository_info.return_value = [200, {"User": "nmirarchi12",
+                                     "Repo: DO180 Number of commits": "1",
+                                     "Repo: DO180-apps Number of commits": "2",
+                                     "Repo: HW-04a Number of commits": "10",
+                                     "Repo: php-app Number of commits": "11",
+                                     "Repo: SSW-567 Number of commits": "7",
+                                     "Repo: Triangle567 Number of commits": "8"}]
+        self.assertTrue(repository_info.connect("mirarni"))
 """
         repo_list = gitRequest.repository_info(Github)
         print(repo_list)
         self.assertIsInstance(repo_list, list)
         for i in repo_list:
             self.assertIsInstance(i, str)
+            User: nmirarchi12
+Repo: DO180 Number of commits: 1
+Repo: DO180-apps Number of commits: 2
+Repo: HW-04a Number of commits: 10
+Repo: php-app Number of commits: 11
+Repo: SSW-567 Number of commits: 7
+Repo: Triangle567 Number of commits: 8
 """
