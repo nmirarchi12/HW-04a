31823f5a9c06d668ff423467203ded9f09193a9c
Nicholas Mirarchi <nmirarchi12@gmail.com>
2021-10-06 15:28:26 -0400

    updating mocking script

--- mocking.py
+++ mocking.py
@@ -4,6 +4,7 @@ Created on Wed Oct  6 09:55:18 2021
 
 @author: nicholas mirarchi
 """
+import unittest
 from unittest.mock import Mock, patch
 from nose.tools import assert_is_not_none
 from gitRequest import repository_info
@@ -11,20 +12,16 @@ import json
 
 Github = 'nmirarchi12'
 RESP = 'True'
-
-@patch('gitRequest.repository_info')
-def test_repository_info(self, mock_repository_info):
-    # Opening JSON file
-    jData = open('json_data_output.json',)
-    # It returns JSON object as dictionary
-    myJson = json.load(jData)
-    mock_repository_info.return_value = Mock(ok=True)
-    mock_repository_info.json.return_value=myJson
-    
-    response = repository_info()
-    
-    assert_list_equal(response.json(),myJson)
-    assert_is_not_none(response)
+class TestHw4(unittest.TestCase):
+    @patch('gitRequest.repository_info')
+    def test_repository_info(self, mock_repository_info):
+        jData = open('json_data_output.json',)
+        myJson = json.load(jData)
+        mock_repository_info.return_value = Mock(ok=True)
+        mock_repository_info.json.return_value=myJson
+        response = repository_info()
+        assert_list_equal(response.json(),myJson)
+        assert_is_not_none(response)
 """
         repo_list = gitRequest.repository_info(Github)
         print(repo_list)
